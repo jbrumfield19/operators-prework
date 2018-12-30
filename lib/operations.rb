ad960042a7205ab788295d0f4a8279c2a222ab87
Jessica Brumfield <jbrumfield1999@gmail.com>
2018-12-30 01:15:53 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -10,8 +10,8 @@ end
 
 def not_safe?(speed)
  speed > 60 ? return true: false
- 	speed < 40 ? return true : false
- 	40 < speed > 60 ? return false : true
+ 	speed < 40 ? return true: false
+ 	40 < speed > 60 ? return false: true
 end
 	
 
