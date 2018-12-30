4d566d5cd17534cdb77e3249aad60cb5fb0b1458
Jessica Brumfield <jbrumfield1999@gmail.com>
2018-12-30 01:08:49 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -9,8 +9,7 @@ end
 
 
 def not_safe?(speed)
-  if speed > 60 || speed < 40
-  return TRUE
+  if speed > 60 || speed < 40 ?: TRUE : FALSE
     elsif speed < 60 || speed > 40
     return FALSE
   end
